69c89e3280c8502a8fd633c5d259ce84402a7c22
Sandul <sandul.20221092@iit.ac.lk>
2026-03-23 16:06:52 +0530

    nn

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // ✅ MUST HAVE
+import 'firebase_options.dart';
 import 'login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform, // ✅ THIS FIXES ERROR
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(const SkinDiseaseApp());
